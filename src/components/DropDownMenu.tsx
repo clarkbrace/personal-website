d28c2fedc1fcef116c7217e4ecbed9409ff1f230
ClarkUPS <cbrace@pugetsound.edu>
2024-02-08 18:05:59 -0800

    Clark website add

---
+++ src/components/DropDownMenu.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const DropDownMenu = () => {
+    return (
+      
+    <div>DropDownMenu</div>
+  )
+}
+
+export default DropDownMenu
---
+++ src/components/MainNavigation.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import React from "react";
+
+const MainNavigation = () => {
+  return (
+    <>
+      <div className="w-full h-32 bg-gray-500 drop-shadow-2xl rounded-bl-lg rounded-br-lg">
+        <div className=" flex justify-center">
+          <h1 className="text-xl flex-none bg-blue-600 border-2 font-serif">
+            Clark's Website
+          </h1>
+        </div>
+        <h1>Test 2</h1>
+        <h1>Test Here</h1>
+      </div>
+    </>
+  );
+};
+
+export default MainNavigation;
