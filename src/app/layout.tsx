3cb5a39002239f5f1f82207ff88ac5c74a343f08
ClarkUPS <cbrace@pugetsound.edu>
2024-04-03 20:57:57 -0700

    changed next/font to  @next/font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "./styles.css";
-import localFont from "next/font/local";
+import localFont from "@next/font/local";
 
 export const minesweeperfont = localFont({
   src: "../../public/fonts/mine-sweeper.otf",
