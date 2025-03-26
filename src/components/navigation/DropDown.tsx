d069cdb4117f97e26da261d07ec865c6879ba90f
clark brace <clarkbrace@gmail.com>
2025-03-26 10:55:49 -0700

    Fixed Drop down menu hover and animaion. Now hovering over the invisable drop down menu items does not caulse the drop down to appear

--- src/components/navigation/DropDown.tsx
+++ src/components/navigation/DropDown.tsx
@@ -1,8 +1,10 @@
 "use client";
-import { useState } from "react";
+import { Children, useState } from "react";
 import DropDownItem from "./DropDownItem";
 import { DropDownMenuItem } from "../../../types";
 import Link from "next/link";
+import { AnimatePresence, motion } from "motion/react";
+import { easeInOut, easeOut } from "motion";
 
 interface Props {
   title: string;
@@ -11,44 +13,50 @@ interface Props {
 }
 
 export default function DropDown(props: Props) {
-  const [showDropDown, setShowDropDown] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const showDropDown = open && props.dropDownItems;
 
   return (
     <div
       className="flex flex-col items-center font-overpassMono px-2"
-      onMouseEnter={() => setShowDropDown(true)}
-      onMouseLeave={() => setShowDropDown(false)}
+      onMouseEnter={() => setOpen(true)}
+      onMouseLeave={() => setOpen(false)}
     >
-      <Link href={ props.dropDownLink ? props.dropDownLink : "" }>
+      <Link href={props.dropDownLink ? props.dropDownLink : ""}>
         <h1 className="text-3xl px-2 pt-0.5 rounded-lg bg-gray-100 text-BodyBlue text-center">
           {props.title}
         </h1>
       </Link>
 
-      {props.dropDownItems && props.dropDownItems.length > 0 && (
-        <div
-          className={`absolute transition-all ease-in-out duration-200 ${
-            showDropDown ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-2"
-          }`}
-        >
-          <div className={`flex flex-col items-center pt-12`}>
-            <div
-              className="w-0 h-0 
-                        border-l-[25px] border-l-transparent
-                        border-b-[18px] 
-                        border-r-[25px] border-r-transparent"
-            ></div>
-            <div className="border-8 rounded-lg">
-              {props.dropDownItems &&
-                props.dropDownItems.map((dropDownMenuItem) => (
-                  <DropDownItem DropDownItem={dropDownMenuItem} key={dropDownMenuItem.label} />
-                ))}
+      <AnimatePresence>
+        {showDropDown && (
+          <motion.div
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -15 }}
+            transition={{ duration: 0.1, ease: easeOut }}
+            className={`absolute`}
+          >
+            <div className={`flex flex-col items-center pt-12`}>
+              <div
+                className="w-0 h-0 translate-y-1 
+                        border-l-[30px] border-l-transparent
+                        border-b-[20px] 
+                        border-r-[30px] border-r-transparent"
+              ></div>
+              <div className="border-8 rounded-lg">
+                {props.dropDownItems &&
+                  props.dropDownItems.map((dropDownMenuItem) => (
+                    <DropDownItem DropDownItem={dropDownMenuItem} key={dropDownMenuItem.label} />
+                  ))}
+              </div>
             </div>
-          </div>
-        </div>
-      )}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
 
-// const DropDown
+const DropdownPanel = {};
